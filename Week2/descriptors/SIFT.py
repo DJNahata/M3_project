43fcdbefbf644a14e97f8bc1744b332955a9a9a1
RogerCasalsVilardell <43968704+RogerCasalsVilardell@users.noreply.github.com>
2020-01-01 18:39:17 +0100

    Added code for 1 or 2 pyramid levels

--- Week2/descriptors/SIFT.py
+++ Week2/descriptors/SIFT.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 
 class DenseSIFT():
@@ -18,4 +19,30 @@ class DenseSIFT():
                         kps.append(kp)
             _, des = self.SIFT.compute(img, kps)
             descriptors.append(des)
+        return descriptors
+
+
+class DenseSIFTPyramid():
+        
+    def __init__(self):
+        self.SIFT = cv2.xfeatures2d.SIFT_create()
+
+    def compute(self, img_paths, step_size, descriptor_sizes, n_partitions=1):
+        descriptors = []
+        for img_path in img_paths:
+            img = cv2.imread(img_path, 0)
+            a = img.shape[1]
+            b = img.shape[0]
+            partitions = [[int(i/n_partitions*a),int((i+1)/n_partitions*a),int(j/n_partitions*b),int((j+1)/n_partitions*b)] for i in range(n_partitions) for j in range(n_partitions)]
+            all_des = []
+            for xmin, xmax, ymin, ymax in partitions:
+                kps = []
+                for x in range(xmin, xmax, step_size):
+                    for y in range(ymin, ymax, step_size):
+                        for descriptor_size in descriptor_sizes:
+                            kp = cv2.KeyPoint(x, y, descriptor_size)
+                            kps.append(kp)
+                _, des = self.SIFT.compute(img, kps)
+                all_des.append(des)
+            descriptors.append(all_des)
         return descriptors
