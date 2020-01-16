19c8c37b67fe900449ee0653932688e281afc970
joanfontanals <jfontanals@ntent.com>
2020-01-16 19:28:31 +0100

    Guarrada check

--- Week3/task4_cvc.py
+++ Week3/task4_cvc.py
@@ -5,12 +5,41 @@ import os
 from sklearn.model_selection import GridSearchCV
 from sklearn.svm import SVC
 
-FEATURE_PATH = "path"
+LAYER_IDX = 1
+BASE_PATH = "task4_data"
 
-train_data_path = FEATURE_PATH
-test_data_path = FEATURE_PATH
+train_data_path = BASE_PATH + "/task2_train_feat.pkl"
+test_data_path = BASE_PATH + "/task2_test_feat.pkl"
+#a = pickle.load(open("/Users/joanfontanals/MasterCVC/M3/Project/M3_project/SavePath/data/test128_8_8.dat", 'rb'))
+#print(type(a))
+#print(a)
+#print(a.shape)
+
 train_data = pickle.load(open(train_data_path,'rb'))
-test_data = pickle.load(open(test_data_path,'rb'))
+print(type(train_data))
+print(type(train_data[0]))
+print(type(train_data[0][0]))
+print(type(train_data[0][0][0]))
+print(len(train_data))
+print(len(train_data[0]))
+print(len(train_data[0][0]))
+print(len(train_data[0][0][0]))
+#print(train_data)
+print(train_data[0])
+print(train_data[0][0])
+print(np.array(train_data))
+test_data = pickle.load(open(test_data_path,'rb'))[LAYER_IDX]
+
+train_labels_path = BASE_PATH + "/task2_train_label.pkl"
+test_labels_path = BASE_PATH + "/task2_test_label.pkl"
+train_labels = pickle.load(open(train_labels_path,'rb'))
+test_labels = pickle.load(open(test_labels_path,'rb'))
+
+print(train_data.shape)
+print(test_data.shape)
+
+print(train_labels.shape)
+print(test_labels.shape)
 
 def histogram_intersection_kernel(a, b):
     K = np.empty(shape=(a.shape[0], b.shape[0]), dtype=np.float32)
@@ -19,7 +48,7 @@ def histogram_intersection_kernel(a, b):
     return K
 
 K_FOLDS = 5
-param_grid = {'C': [0.001389], 'kernel': ['rbf', histogram_intersection_kernel], 'gamma': ['scale'], 'tol': [1.34590]}
+PARAM_GRID = {'C': [0.001389], 'kernel': ['rbf', histogram_intersection_kernel], 'gamma': ['scale'], 'tol': [1.34590]}
 
 cv = GridSearchCV(SVC(), param_grid=PARAM_GRID, cv=K_FOLDS)
 cv.fit(train_data, train_labels)
