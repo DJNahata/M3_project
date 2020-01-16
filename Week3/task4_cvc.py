65b5d46584f534c301c0f6e6cf71ff860e1c728d
joanfontanals <jfontanals@ntent.com>
2020-01-16 19:45:22 +0100

    Working task4

--- Week3/task4_cvc.py
+++ Week3/task4_cvc.py
@@ -11,45 +11,27 @@ BASE_PATH = "task4_data"
 
 train_data_path = BASE_PATH + "/task2_train_feat.pkl"
 test_data_path = BASE_PATH + "/task2_test_feat.pkl"
-#a = pickle.load(open("/Users/joanfontanals/MasterCVC/M3/Project/M3_project/SavePath/data/test128_8_8.dat", 'rb'))
-#print(type(a))
-#print(a)
-#print(a.shape)
 
 train_data = pickle.load(open(train_data_path,'rb'))
-print(type(train_data))
-print(type(train_data[0]))
-print(type(train_data[0][0]))
-print(type(train_data[0][0][0]))
-print(len(train_data))
-print(len(train_data[0]))
-print(len(train_data[0][0]))
-print(len(train_data[0][0][0]))
-#print(train_data)
-print(train_data[0])
-print(train_data[0][0])
-print(np.array(train_data))
-test_data = pickle.load(open(test_data_path,'rb'))[LAYER_IDX]
+feature_train = np.array([item[LAYER_IDX][0] for item in train_data])
+test_data = pickle.load(open(test_data_path,'rb'))
+feature_test = np.array([item[LAYER_IDX][0] for item in test_data])
 
 train_labels_path = BASE_PATH + "/task2_train_label.pkl"
 test_labels_path = BASE_PATH + "/task2_test_label.pkl"
 train_labels = pickle.load(open(train_labels_path,'rb'))
 test_labels = pickle.load(open(test_labels_path,'rb'))
 
-print(train_data.shape)
-print(test_data.shape)
-
-print(train_labels.shape)
-print(test_labels.shape)
-
+print(feature_train.shape)
+print(len(train_labels))
 K_FOLDS = 5
 PARAM_GRID = {'C': [0.001389], 'kernel': ['rbf', histogram_intersection_kernel], 'gamma': ['scale'], 'tol': [1.34590]}
 
-cv = GridSearchCV(SVC(), param_grid=PARAM_GRID, cv=K_FOLDS)
-cv.fit(train_data, train_labels)
+cv = GridSearchCV(SVC(), param_grid=PARAM_GRID, cv=K_FOLDS, n_jobs=-1, verbose=5)
+cv.fit(feature_train, train_labels)
 
-train_score = cv.score(train_data, train_labels)
-test_score  = cv.score(test_data, test_labels)
+train_score = cv.score(feature_train, train_labels)
+test_score  = cv.score(feature_test, test_labels)
 
 print("Train accuracy score: {}\nTest accuracy score: {}\nBest params: {}\n".format(train_score, test_score, cv.best_params_))
 print("All results: {}".format(cv.cv_results_))
