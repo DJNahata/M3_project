ce184d42e85dc26a69ec59366cd86003bf2840af
joanfontanals <jfontanals@ntent.com>
2020-01-17 09:17:14 +0100

    Better model task4

--- Week3/task4_cvc.py
+++ Week3/task4_cvc.py
@@ -6,7 +6,7 @@ from utils.Kernels import histogram_intersection_kernel
 from sklearn.model_selection import GridSearchCV
 from sklearn.svm import SVC
 
-LAYER_IDX = 1
+LAYER_IDX = 2
 BASE_PATH = "task4_data"
 
 train_data_path = BASE_PATH + "/task2_train_feat.pkl"
@@ -25,7 +25,7 @@ test_labels = pickle.load(open(test_labels_path,'rb'))
 print(feature_train.shape)
 print(len(train_labels))
 K_FOLDS = 5
-PARAM_GRID = {'C': [0.001389], 'kernel': ['rbf', histogram_intersection_kernel], 'gamma': ['scale'], 'tol': [1.34590]}
+PARAM_GRID = {'C': [0.001389, 0.01, 0.1, 1, 10], 'kernel': ['rbf', histogram_intersection_kernel], 'gamma': [1e-3, 1e-4, 'scale']}
 
 cv = GridSearchCV(SVC(), param_grid=PARAM_GRID, cv=K_FOLDS, n_jobs=-1, verbose=5)
 cv.fit(feature_train, train_labels)
@@ -35,3 +35,8 @@ test_score  = cv.score(feature_test, test_labels)
 
 print("Train accuracy score: {}\nTest accuracy score: {}\nBest params: {}\n".format(train_score, test_score, cv.best_params_))
 print("All results: {}".format(cv.cv_results_))
+
+#Best so far:
+#Train accuracy score: 0.9994683678894205
+#Test accuracy score: 0.6827757125154895
+#Best params: {'C': 0.001389, 'gamma': 0.001, 'kernel': <function histogram_intersection_kernel at 0x102e6e680>}
