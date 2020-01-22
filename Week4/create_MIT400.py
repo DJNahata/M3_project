d5be60ad049067cb4cc856ce3b4c759300a8379f
RogerCasalsVilardell <43968704+RogerCasalsVilardell@users.noreply.github.com>
2020-01-22 10:30:42 +0100

    Corrections

--- Week4/create_MIT400.py
+++ Week4/create_MIT400.py
@@ -4,8 +4,8 @@ import shutil
 import os
 
 if __name__ == "__main__":
-    data_dir = '/home/mcv/datasets//MIT_split'
-    save_dir = '/home/grupo07/datasets//MIT_400'
+    data_dir = '/home/mcv/datasets/MIT_split'
+    save_dir = '/home/grupo07/datasets/MIT_400'
     classes = ['coast','forest','highway','inside_city','mountain','Opencountry','street','tallbuilding']
     if not os.path.isdir(save_dir):
         os.makedirs(save_dir+os.sep+'train')
@@ -15,9 +15,18 @@ if __name__ == "__main__":
     for category in classes:
         images = glob(data_dir+os.sep+'train'+os.sep+category+os.sep+'*.jpg')
         shuffle(images)
-        os.makedirs(save_dir+os.sep+'train'+os.sep+category)
-        os.makedirs(save_dir+os.sep+'validation'+os.sep+category)
+        if not os.path.isdir(save_dir+os.sep+'train'+os.sep+category):
+            os.makedirs(save_dir+os.sep+'train'+os.sep+category)
+        if not os.path.isdir(save_dir+os.sep+'validation'+os.sep+category):
+            os.makedirs(save_dir+os.sep+'validation'+os.sep+category)
         for img_path in images[:50]:
             shutil.copy(img_path, save_dir+os.sep+'train'+os.sep+category)
         for img_path in images[50:100]:
             shutil.copy(img_path, save_dir+os.sep+'validation'+os.sep+category)
+
+        images = glob(data_dir+os.sep+'test'+os.sep+category+os.sep+'*.jpg')
+        if not os.path.isdir(save_dir+os.sep+'test'+os.sep+category):
+            os.makedirs(save_dir+os.sep+'test'+os.sep+category)
+        for img_path in images:
+            shutil.copy(img_path, save_dir+os.sep+'test'+os.sep+category)
+
--- Week4/main.py
+++ Week4/main.py
@@ -6,25 +6,26 @@ from utils.Data import DataRetrieval
 from utils.Model import NasNetMob
 
 
-data_dir = '/home/mcv/datasets/MIT_split'
+data_dir = '/home/grupo07/datasets/MIT_400'
+work_dir = '/home/grupo07/work'
 img_width = 224
 img_height = 224
 batch_size = 16
 number_of_epoch = 20
-validation_samples = 807
+validation_samples = 400
+test_samples = 807
 
 # Create model
 model = NasNetMob()    
     
 model.compile(loss='categorical_crossentropy',optimizer=Adam(lr=0.001), metrics=['accuracy'])
 for layer in model.layers:
-    print layer.name, layer.trainable
+    print(layer.name, layer.trainable)
 
 # Prepare data
 DR = DataRetrieval(data_dir)
 train_generator = DR.get_train_data(batch_size=batch_size)
-# validation_generator = DR.get_validation_data(batch_size=batch_size)
-validation_generator = DR.get_test_data(batch_size=batch_size)
+validation_generator = DR.get_validation_data(batch_size=batch_size)
 test_generator = DR.get_test_data(batch_size=batch_size)
 
 history=model.fit_generator(
@@ -35,8 +36,8 @@ history=model.fit_generator(
     validation_steps= (int(validation_samples//batch_size)+1))
 
 
-result = model.evaluate_generator(test_generator, val_samples=validation_samples)
-print result
+result = model.evaluate_generator(test_generator, val_samples=test_samples)
+print(result)
 
 if True:
     # summarize history for accuracy
@@ -46,7 +47,7 @@ if True:
     plt.ylabel('accuracy')
     plt.xlabel('epoch')
     plt.legend(['train', 'validation'], loc='upper left')
-    plt.savefig('accuracy.jpg')
+    plt.savefig(work_dir+os.sep+'accuracy.jpg')
     plt.close()
     # summarize history for loss
     plt.plot(history.history['loss'])
@@ -55,4 +56,4 @@ if True:
     plt.ylabel('loss')
     plt.xlabel('epoch')
     plt.legend(['train', 'validation'], loc='upper left')
-    plt.savefig('loss.jpg')
+    plt.savefig(work_dir+os.sep+'loss.jpg')
