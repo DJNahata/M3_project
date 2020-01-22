3bf4033c67073d925a836ab1bc6d3270af6bea2a
RogerCasalsVilardell <43968704+RogerCasalsVilardell@users.noreply.github.com>
2020-01-22 11:26:49 +0100

    Corrections

--- Week4/main.py
+++ Week4/main.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import matplotlib.pyplot as plt
 
 from keras.optimizers import Adam
@@ -12,57 +13,68 @@ work_dir = '/home/grupo07/work'
 img_width = 224
 img_height = 224
 batch_size = 16
-number_of_epoch = 20
+epochs = 50
 train_samples = 400
 validation_samples = 400
 test_samples = 807
 
-# Hyperparameters
-learning_rate = 1e-3
-optimizer = Adam(lr=learning_rate)
-dropout = 0.5
-weight_decay = 5e-5
+# Run experiments
+learning_rates = [item+abs(np.random.normal(0,item)) for item in np.logspace(-5,0,6)]
+for ind, learning_rate in enumerate(learning_rates):
 
-# Create model
-nasnetmob = NasNetMob(dropout=dropout, weight_decay=weight_decay)
-nasnetmob.freeze()
-nasnetmob.model.compile(loss='categorical_crossentropy',optimizer=optimizer, metrics=['accuracy'])
-for layer in nasnetmob.model.layers:
-    print(layer.name, layer.trainable)
+    # Hyperparameters
+    optimizer = Adam(lr=learning_rate)
+    dropout = 0.5
+    weight_decay = 5e-5
 
-# Prepare data
-DR = DataRetrieval(data_dir)
-train_generator = DR.get_train_data(batch_size=batch_size)
-validation_generator = DR.get_validation_data(batch_size=batch_size)
-test_generator = DR.get_test_data(batch_size=batch_size)
+    # Create model
+    nasnetmob = NasNetMob(dropout=dropout, weight_decay=weight_decay)
+    nasnetmob.freeze()
+    nasnetmob.model.compile(loss='categorical_crossentropy',optimizer=optimizer, metrics=['accuracy'])
+    # for layer in nasnetmob.model.layers:
+    #     print(layer.name, layer.trainable)
 
-history = nasnetmob.model.fit_generator(
-    train_generator,
-    steps_per_epoch=(int(train_samples//batch_size)+1),
-    nb_epoch=number_of_epoch,
-    validation_data=validation_generator,
-    validation_steps= (int(validation_samples//batch_size)+1))
+    # Prepare data
+    DR = DataRetrieval(data_dir)
+    train_generator = DR.get_train_data(batch_size=batch_size)
+    validation_generator = DR.get_validation_data(batch_size=batch_size)
+    test_generator = DR.get_test_data(batch_size=batch_size)
+
+    history = nasnetmob.model.fit_generator(
+        train_generator,
+        steps_per_epoch=(int(train_samples//batch_size)+1),
+        nb_epoch=epochs,
+        validation_data=validation_generator,
+        validation_steps= (int(validation_samples//batch_size)+1))
 
 
-result = nasnetmob.model.evaluate_generator(test_generator, val_samples=test_samples)
-print('\nTest loss:',result[0])
-print('Test accuracy:',result[1])
+    result = nasnetmob.model.evaluate_generator(test_generator, val_samples=test_samples)
+    print('\nTest loss:',result[0])
+    print('Test accuracy:',result[1])
+    with open(work_dir+os.sep+str(ind)+'_info.txt','w') as f:
+        to_write = 'Learning rate: '+str(learning_rate)+\
+            '\nDropout: '+str(dropout)+\
+            '\nWeight decay: '+str(weight_decay)+\
+            '\nTest loss: '+str(result[0])+\
+            '\nTest accuracy: '+str(result[1])+\
+            '\n'
+        f.write(to_write)
 
-if True:
-    # summarize history for accuracy
-    plt.plot(history.history['acc'])
-    plt.plot(history.history['val_acc'])
-    plt.title('model accuracy')
-    plt.ylabel('accuracy')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'validation'], loc='upper left')
-    plt.savefig(work_dir+os.sep+'accuracy.jpg')
-    plt.close()
-    # summarize history for loss
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('model loss')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'validation'], loc='upper left')
-    plt.savefig(work_dir+os.sep+'loss.jpg')
+    if True:
+        # summarize history for accuracy
+        plt.plot(history.history['acc'])
+        plt.plot(history.history['val_acc'])
+        plt.title('model accuracy')
+        plt.ylabel('accuracy')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'validation'], loc='upper left')
+        plt.savefig(work_dir+os.sep+str(ind)+'_accuracy.jpg')
+        plt.close()
+        # summarize history for loss
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        plt.title('model loss')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'validation'], loc='upper left')
+        plt.savefig(work_dir+os.sep+str(ind)+'_loss.jpg')
