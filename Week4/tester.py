89071056a9e8a9c18bb7c41512bd74f2e4aaa99c
paudom <pavdom72@gmail.com>
2020-01-24 19:52:01 +0100

    TESTER ADDED

--- Week4/tester.py
+++ Week4/tester.py
@@ -9,26 +9,29 @@ from utils.Model import NasNetMob
 
 # -- CONSTANTS -- #
 DATA_DIR = '/home/grupo07/datasets/MIT_400'
-WORK_DIR = '/home/grupo07/work'
-NAME = 'experiment'
+WORK_DIR = '/home/grupo07/task3_work_DA_NReg'
+NAME = 'experiment1'
 
 # -- CONFIG DICTIONARY -- #
 BATCH_SIZE = 16
 BATCH_NORM = False
-UNITS_2DENSE = 512 # int to activate
-LR = 1E-3
-UNFREEZE_LR = 1e-5
-DA_AUG = True
-TRAIN_SAMPLES = 400
-VAL_SAMPLES= 400
-TEST_SAMPLES = 807
-RUN_AFTER_FREEZE = False
-EPOCHS = 20
-OPTIMIZER = SGD
-MOMENTUM = 0.9 
-NESTEROV = True
+UNITS_2DENSE = None # int to activate
 DROPOUT = None # float to enable
 WEIGHT_DECAY = None # float to enable
+LR = 1E-3
+EARLY_PATIENCE = 10
+REDUCE_PATIENCE = 10
+REDUCE_FACTOR = 0.1
+UNFREEZE_LR = 1e-5
+DA_AUG = True
+TRAIN_SAMPLES = 10*400
+VAL_SAMPLES= 10*400
+TEST_SAMPLES = 807
+RUN_AFTER_FREEZE = False
+EPOCHS = 30
+OPTIMIZER = 'Adam'
+MOMENTUM = 0.9 
+NESTEROV = True
 
 if __name__ == "__main__":
 
@@ -36,30 +39,37 @@ if __name__ == "__main__":
         os.mkdir(WORK_DIR)
 
     print('Loading Data...')
-    DR = DataRetrieval(data_dir)
-    train_generator = DR.get_train_data(augmentation=DA_AUG, batch_size=batch_size)
-    val_generator = DR.get_validation_data(batch_size=batch_size)
-    test_generator = DR.get_test_data(batch_size=batch_size)
+    DR = DataRetrieval(DATA_DIR)
+    train_generator = DR.get_train_data(augmentation=DA_AUG, batch_size=BATCH_SIZE)
+    val_generator = DR.get_validation_data(batch_size=BATCH_SIZE)
+    test_generator = DR.get_test_data(batch_size=BATCH_SIZE)
     print('Loaded.')
 
     print('Loading Model...')
     nasnetmob = NasNetMob(dense=UNITS_2DENSE, batchnorm=BATCH_NORM, dropout=DROPOUT, weight_decay=WEIGHT_DECAY)
     nasnetmob.freeze()
+    early = EarlyStopping(monitor='val_loss',patience=EARLY_PATIENCE)
+    reduce_lr = ReduceLROnPlateau(monitor='val_loss',factor=REDUCE_FACTOR, patience=REDUCE_PATIENCE)
     print('Loaded.')
 
     print('Training...')
-    if OPTIMIZER is SGD:
-        optimizer = OPTIMIZER(learning_rate=LR, momentum=MOMENTUM, nesterov=NESTEROV)
+    if OPTIMIZER == 'Adam':
+        optimizer = Adam(lr=LR)
+    elif OPTIMIZER == 'Adadelta':
+        optimizer = Adadelta(lr=LR)
+    elif OPTIMIZER == 'RMSprop':
+        optimizer = RMSprop(lr=LR)
     else:
-        optimizer = OPTIMIZER(learning_rate=LR)
+        optimizer = SGD(lr=LR, momentum=MOMENTUM, nesterov=NESTEROV)
     
     nasnetmob.model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
-    history = model.fit_generator(
+    history = nasnetmob.model.fit_generator(
         train_generator,
         steps_per_epoch=(int(TRAIN_SAMPLES // BATCH_SIZE) + 1),
         nb_epoch=EPOCHS,
         validation_data=val_generator,
-        validation_steps=(int(VAL_SAMPLES // BATCH_SIZE) + 1)
+        validation_steps=(int(VAL_SAMPLES // BATCH_SIZE) + 1),
+        callbacks = [early, reduce_lr]
     )
     print('Plotting...')
     plt.plot(history.history['acc'])
@@ -82,31 +92,36 @@ if __name__ == "__main__":
 
     result = nasnetmob.model.evaluate_generator(test_generator, val_samples=TEST_SAMPLES)
     with open(WORK_DIR + os.sep + str(NAME)+'freezed_info.txt', 'w') as f:
-        to_write = 'Optimizer: ' + OPTIMIZER.__class__.__name__ + \
-                    '\nLearning rate: ' + str(LR) + \
-                    '\nBatch_size: ' + str(BATCH_SIZE) + \   
-                    '\nDropout: ' + str(DROPOUT) + \
-                    '\nBatchNorm: ' + str(BATCH_NORM) + \
-                    '\nWeight decay: ' + str(WEIGHT_DECAY) + \
-                    '\nTest loss: ' + str(result[0]) + \
-                    '\nTest accuracy: ' + str(result[1]) + \
-                    '\nEpochs: ' + str(EPOCHS) + \
-                    '\n'
+        to_write = 'Optimizer: ' + optimizer_name + \
+                       '\nLearning rate: ' + str(LR) + \
+                       '\nDropout: ' + str(DROPOUT) + \
+                       '\nWeight decay: ' + str(WEIGHT_DECAY) + \
+                       '\nBatch_size: ' + str(BATCH_SIZE) + \
+                       '\nBatchNorm: ' + str(BATCH_NORM) + \
+                       '\nTest loss: ' + str(result[0]) + \
+                       '\nTest accuracy: ' + str(result[1]) + \
+                       '\nEpochs: ' + str(EPOCHS) + \
+                       '\n'
         f.write(to_write)
 
     if RUN_AFTER_FREEZE:
         nasnetmob.unfreeze()
-        if OPTIMIZER is SGD:
-            optimizer = OPTIMIZER(learning_rate=UNFREEZE_LR, momentum=MOMENTUM, nesterov=NESTEROV)
+        if OPTIMIZER == 'Adam':
+            optimizer = Adam(lr=LR)
+        elif OPTIMIZER == 'Adadelta':
+            optimizer = Adadelta(lr=LR)
+        elif OPTIMIZER == 'RMSprop':
+            optimizer = RMSprop(lr=LR)
         else:
-            optimizer = OPTIMIZER(learning_rate=UNFREEZE_LR)
+            optimizer = SGD(lr=LR, momentum=MOMENTUM, nesterov=NESTEROV)
         nasnetmob.model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
-        history = model.fit_generator(
+        history = nasnetmob.model.fit_generator(
             train_generator,
             steps_per_epoch=(int(TRAIN_SAMPLES // BATCH_SIZE) + 1),
             nb_epoch=EPOCHS,
             validation_data=val_generator,
-            validation_steps=(int(VAL_SAMPLES // BATCH_SIZE) + 1)
+            validation_steps=(int(VAL_SAMPLES // BATCH_SIZE) + 1),
+            callbacks = [early, reduce_lr]
         )
         print('Plotting...')
         plt.plot(history.history['acc'])
@@ -129,14 +144,14 @@ if __name__ == "__main__":
 
         result = nasnetmob.model.evaluate_generator(test_generator, val_samples=TEST_SAMPLES)
         with open(WORK_DIR + os.sep + str(NAME)+'unfreezed_info.txt', 'w') as f:
-            to_write = 'Optimizer: ' + OPTIMIZER.__class__.__name__ + \
-                        '\nLearning rate: ' + str(LR) + \
-                        '\nBatch_size: ' + str(BATCH_SIZE) + \   
-                        '\nDropout: ' + str(DROPOUT) + \
-                        '\nBatchNorm: ' + str(BATCH_NORM) + \
-                        '\nWeight decay: ' + str(WEIGHT_DECAY) + \
-                        '\nTest loss: ' + str(result[0]) + \
-                        '\nTest accuracy: ' + str(result[1]) + \
-                        '\nEpochs: ' + str(EPOCHS) + \
-                        '\n'
+            to_write = 'Optimizer: ' + optimizer_name + \
+                       '\nLearning rate: ' + str(LR) + \
+                       '\nDropout: ' + str(DROPOUT) + \
+                       '\nWeight decay: ' + str(WEIGHT_DECAY) + \
+                       '\nBatch_size: ' + str(BATCH_SIZE) + \
+                       '\nBatchNorm: ' + str(BATCH_NORM) + \
+                       '\nTest loss: ' + str(result[0]) + \
+                       '\nTest accuracy: ' + str(result[1]) + \
+                       '\nEpochs: ' + str(EPOCHS) + \
+                       '\n'
             f.write(to_write)
